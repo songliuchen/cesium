7a9bc0e10ab272ae321dc4f3aee0b166e26ac123
songlc@powerkeen.com <songlc@powerkeen.com>
2019-07-02 21:02:28 +0800

    修改cesium 版本

---
+++ assets/plugins/cesium.navigation.js
@@ -0,0 +1,16 @@
+/**
+ * Cesium 仿超图导航控件
+ */
+var CesiumNavigation=(function (){
+    /**
+     * 调用加载导航方法
+     * @param {*} viewer  
+     */
+    function load(viewer,options)
+    {
+        
+    }
+    return {
+        load:load
+    }
+ })()
